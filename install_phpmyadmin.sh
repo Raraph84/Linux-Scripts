e430e1488f7e38055a531689c85d9174ded7b906
Raraph84 <raraph84@gmail.com>
2020-12-13 17:35:23 +0100

    opti toinux

--- install_phpmyadmin.sh
+++ install_phpmyadmin.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Vérifier si le script est lancé en root
+if [ "$UID" -ne "0" ]
+then
+   echo "Merci de lancer le script en root !"
+   exit 1
+fi
+
 # Demander mot de passe root (MySQL)
 read -sp "Tapez votre mot de passe root (MySQL) : " ROOTPASS
 echo ""
@@ -8,34 +15,34 @@ echo ""
 wget -q https://files.phpmyadmin.net/phpMyAdmin/5.0.4/phpMyAdmin-5.0.4-all-languages.zip
 
 # Dézipper
-sudo unzip -q phpMyAdmin-5.0.4-all-languages.zip -d /opt
+unzip -q phpMyAdmin-5.0.4-all-languages.zip -d /opt
 
 # Supprimer l'archive
 rm phpMyAdmin-5.0.4-all-languages.zip
 
 # Déplacer et donner les permissions
-sudo mv /opt/phpMyAdmin-5.0.4-all-languages /opt/phpmyadmin
-sudo chown -R www-data:www-data /opt/phpmyadmin
+mv /opt/phpMyAdmin-5.0.4-all-languages /opt/phpmyadmin
+chown -R www-data:www-data /opt/phpmyadmin
 
 # Créer le fichier de configuration
-sudo cp /opt/phpmyadmin/config.sample.inc.php /opt/phpmyadmin/config.inc.php
+cp /opt/phpmyadmin/config.sample.inc.php /opt/phpmyadmin/config.inc.php
 
 # Activer le système d'utilisateur de stockage
 STRING1="\/\/ \$cfg\['Servers'\]\[\$i\]"
 STRING2="\$cfg\['Servers'\]\[\$i\]"
-sudo sed -i -e "s/$STRING1/$STRING2/g" /opt/phpmyadmin/config.inc.php
+sed -i -e "s/$STRING1/$STRING2/g" /opt/phpmyadmin/config.inc.php
 
 # Mot de passe du stockage
 PMAPASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-sudo sed -i -e "s/pmapass/$PMAPASS/g" /opt/phpmyadmin/config.inc.php
+sed -i -e "s/pmapass/$PMAPASS/g" /opt/phpmyadmin/config.inc.php
 
 # Désactiver les lignes qui ne servent pas
 STRING1="\$cfg\['Servers'\]\[\$i\]\['controlhost'\] = '';"
 STRING2="\/\/ \$cfg\['Servers'\]\[\$i\]\['controlhost'\] = '';"
-sudo sed -i -e "s/$STRING1/$STRING2/g" /opt/phpmyadmin/config.inc.php
+sed -i -e "s/$STRING1/$STRING2/g" /opt/phpmyadmin/config.inc.php
 STRING1="\$cfg\['Servers'\]\[\$i\]\['controlport'\] = '';"
 STRING2="\/\/ \$cfg\['Servers'\]\[\$i\]\['controlport'\] = '';"
-sudo sed -i -e "s/$STRING1/$STRING2/g" /opt/phpmyadmin/config.inc.php
+sed -i -e "s/$STRING1/$STRING2/g" /opt/phpmyadmin/config.inc.php
 
 # Créer le stockage et son utilisateur
 mysql --user=root --password=$ROOTPASS < /opt/phpmyadmin/sql/create_tables.sql
@@ -45,20 +52,20 @@ mysql --user=root --password=$ROOTPASS -e "GRANT SELECT, INSERT, UPDATE, DELETE 
 COOKIESECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 STRING1="\$cfg\['blowfish_secret'\] = '';"
 STRING2="\$cfg\['blowfish_secret'\] = '$COOKIESECRET';"
-sudo sed -i -e "s/$STRING1/$STRING2/g" /opt/phpmyadmin/config.inc.php
+sed -i -e "s/$STRING1/$STRING2/g" /opt/phpmyadmin/config.inc.php
 
 # Ajouter le dossier des fichiers temporaires, et le configurer
-sudo mkdir -p /var/lib/phpmyadmin/tmp
-sudo chown -R www-data:www-data /var/lib/phpmyadmin
+mkdir -p /var/lib/phpmyadmin/tmp
+chown -R www-data:www-data /var/lib/phpmyadmin
 STRING="\n\$cfg['TempDir'] = '/var/lib/phpmyadmin/tmp';"
-echo "$STRING" | sudo tee -a /opt/phpmyadmin/config.inc.php > /dev/null
+echo "$STRING" >> /opt/phpmyadmin/config.inc.php
 
 # Créer la configuration apache2 et l'activer
 STRING="Alias /phpmyadmin /opt/phpmyadmin\n\n<Directory /opt/phpmyadmin>\n\n  Options FollowSymLinks\n\n  AllowOverride all\n\n  Require all granted\n\n</Directory>"
-echo "$STRING" | sudo tee /etc/apache2/conf-available/phpmyadmin.conf > /dev/null
-sudo a2enconf phpmyadmin.conf > /dev/null
+echo "$STRING" > /etc/apache2/conf-available/phpmyadmin.conf
+a2enconf phpmyadmin.conf > /dev/null
 
 # Redémarrer apache2
-sudo systemctl restart apache2
+systemctl restart apache2
 
 echo "PhpMyAdmin a été installé avec succès !"
--- uninstall_phpmyadmin.sh
+++ uninstall_phpmyadmin.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+# Vérifier si le script est lancé en root
+if [ "$UID" -ne "0" ]
+then
+   echo "Merci de lancer le script en root !"
+   exit 1
+fi
+
 # Demander mot de passe root (MySQL)
 read -sp "Tapez votre mot de passe root (MySQL) : " ROOTPASS
 echo ""
 
 # Désactiver la configuration apache2
-sudo a2disconf phpmyadmin.conf > /dev/null
+a2disconf phpmyadmin.conf > /dev/null
 
 # Supprimer les fichiers
-sudo rm -rf /opt/phpmyadmin/ /etc/apache2/conf-available/phpmyadmin.conf /var/lib/phpmyadmin
+rm -rf /opt/phpmyadmin/ /etc/apache2/conf-available/phpmyadmin.conf /var/lib/phpmyadmin
 
 # Redémarrer apache2
-sudo systemctl restart apache2
+systemctl restart apache2
 
 # Supprimer le stockage MySQL
 mysql --user=root --password=$ROOTPASS -e "DROP DATABASE phpmyadmin;"
