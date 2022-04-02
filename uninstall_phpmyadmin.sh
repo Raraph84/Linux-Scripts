d6325d79d8ce38be36cc89dbac2d820cd9a1b20f
Raraph84 <raraph84@gmail.com>
2022-04-02 18:10:52 +0200

    Update uninstall_phpmyadmin.sh

--- uninstall_phpmyadmin.sh
+++ uninstall_phpmyadmin.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
-# Vérifier si le script est lancé en root
-if [ "$UID" -ne "0" ]
-then
+if [ "$UID" -ne "0" ]; then
    echo "Merci de lancer le script en root !"
    exit 1
 fi
 
-# Demander mot de passe root (MySQL)
 read -sp "Tapez votre mot de passe root (MySQL) : " ROOTPASS
 echo ""
 
-# Désactiver la configuration apache2
 a2disconf phpmyadmin.conf > /dev/null
 
-# Supprimer les fichiers
-rm -rf /opt/phpmyadmin/ /etc/apache2/conf-available/phpmyadmin.conf /var/lib/phpmyadmin
+rm -rf /opt/phpmyadmin /etc/apache2/conf-available/phpmyadmin.conf /var/lib/phpmyadmin
 
-# Redémarrer apache2
 systemctl restart apache2
 
-# Supprimer le stockage MySQL
 mysql --user=root --password=$ROOTPASS -e "DROP DATABASE phpmyadmin;"
 mysql --user=root --password=$ROOTPASS -e "DROP USER 'pma'@'localhost';"
 
